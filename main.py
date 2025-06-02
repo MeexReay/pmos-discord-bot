df14120f5daa1d15018fc3e642691ce57596d33f
MeexReay <meexreay@gmail.com>
2025-06-02 23:55:11 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+bot.run(open("token", "r").read())
